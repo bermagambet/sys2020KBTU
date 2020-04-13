2df303369aa423f20cd3dff1d429ab15dc338edd
bermagambet <36257463+bermagambet@users.noreply.github.com>
2020-04-13 15:21:08 +0500

    Add files via upload

--- lab10/slab_labwork/lab.c
+++ lab10/slab_labwork/lab.c
@@ -12,19 +12,23 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
+
+
 #define PAGE_SIZE 4096
+
 struct mystruct {
    unsigned int arrayOfIntegers[8];
 };
 
 
-static int param = 2000; // any premade default value
+static int param = 2000; 
 module_param(param, int, 0660);
 
-struct kmem_cache *km; //slab allocation for memory allocation process
+// if successful and NULL if not.
+struct kmem_cache *km;
 void kmallocAlloc(int size) {
    struct timespec t1, t2;
-   //just considering the documentation, putting arguments inside the kmem_cache_create
+
    km = kmem_cache_create("slabAllocation", size, 0, 
                         SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_POISON|SLAB_RED_ZONE, 
                         NULL);
@@ -36,7 +40,7 @@ void kmallocAlloc(int size) {
        kmem_cache_free(km, buf);
    }
    getnstimeofday(&t2);
-   printk(KERN_INFO "Start: %lu. Finish: %lu. Throught: %lu.", t1.tv_nsec, t2.tv_nsec, t2.tv_nsec - t1.tv_nsec);
+   printk(KERN_INFO "Time when we started: %lu. Time when we finished: %lu. How much did it take: %lu.", t1.tv_nsec, t2.tv_nsec, t2.tv_nsec - t1.tv_nsec);
 }
 
 int init_module(void) {
@@ -46,7 +50,7 @@ int init_module(void) {
    struct mystruct m;
    int temp1 = PAGE_SIZE/sizeof(m);
    printk(KERN_INFO "Starting part 2:");
-   printk(KERN_INFO "Page Size: %d. Struct Size: %d. Page size/struct instances: %d.\n", PAGE_SIZE, sizeof(m), temp1);
+   printk(KERN_INFO "The page size is: %d. Size of my struct: %d. Number if instances of my struct would fit in with in a single kernel memory page: %d.\n", PAGE_SIZE, sizeof(m), temp1);
    printk(KERN_INFO "Starting part 3:");
    kmallocAlloc(sizeof(m));
    return 0;
